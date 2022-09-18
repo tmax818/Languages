514d3a0ca3356020043dae74c2232890a3a2f5ae
tmax818 <tylermaxwell661@gmail.com>
2022-09-18 16:27:46 -0700

    init commit

---
+++ src/test/java/tylermaxwell/languages/LanguagesApplicationTests.java
@@ -0,0 +1,13 @@
+package tylermaxwell.languages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguagesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
